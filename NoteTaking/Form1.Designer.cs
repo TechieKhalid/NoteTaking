7a078544d19952e124a41a9959a075eae4f60ec2
Techie <albarnawy1997@gmail.com>
2023-08-16 18:18:21 +0300

    Add project files.

---
+++ NoteTaking/Form1.Designer.cs
@@ -0,0 +1,171 @@
+﻿namespace NoteTaking
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            label2 = new Label();
+            txtTitle = new TextBox();
+            txtMessage = new TextBox();
+            dataGridView1 = new DataGridView();
+            btnNew = new Button();
+            btnSave = new Button();
+            btnRead = new Button();
+            btnDelete = new Button();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(57, 45);
+            label1.Name = "label1";
+            label1.Size = new Size(38, 20);
+            label1.TabIndex = 0;
+            label1.Text = "Title";
+            label1.Click += label1_Click;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(42, 90);
+            label2.Name = "label2";
+            label2.Size = new Size(67, 20);
+            label2.TabIndex = 1;
+            label2.Text = "Message";
+            label2.Click += label2_Click;
+            // 
+            // txtTitle
+            // 
+            txtTitle.Location = new Point(131, 38);
+            txtTitle.Name = "txtTitle";
+            txtTitle.Size = new Size(212, 27);
+            txtTitle.TabIndex = 2;
+            txtTitle.TextChanged += txtTitle_TextChanged;
+            // 
+            // txtMessage
+            // 
+            txtMessage.Location = new Point(131, 87);
+            txtMessage.Multiline = true;
+            txtMessage.Name = "txtMessage";
+            txtMessage.Size = new Size(212, 193);
+            txtMessage.TabIndex = 3;
+            txtMessage.TextChanged += textBox2_TextChanged;
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.AllowUserToAddRows = false;
+            dataGridView1.AllowUserToDeleteRows = false;
+            dataGridView1.AllowUserToResizeColumns = false;
+            dataGridView1.AllowUserToResizeRows = false;
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(425, 38);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.ReadOnly = true;
+            dataGridView1.RowHeadersVisible = false;
+            dataGridView1.RowHeadersWidth = 51;
+            dataGridView1.RowTemplate.Height = 29;
+            dataGridView1.Size = new Size(300, 242);
+            dataGridView1.TabIndex = 4;
+            dataGridView1.CellContentClick += dataGridView1_CellContentClick;
+            // 
+            // btnNew
+            // 
+            btnNew.Location = new Point(131, 329);
+            btnNew.Name = "btnNew";
+            btnNew.Size = new Size(94, 29);
+            btnNew.TabIndex = 5;
+            btnNew.Text = "New";
+            btnNew.UseVisualStyleBackColor = true;
+            btnNew.Click += btnNew_Click;
+            // 
+            // btnSave
+            // 
+            btnSave.Location = new Point(249, 329);
+            btnSave.Name = "btnSave";
+            btnSave.Size = new Size(94, 29);
+            btnSave.TabIndex = 6;
+            btnSave.Text = "Save";
+            btnSave.UseVisualStyleBackColor = true;
+            btnSave.Click += btnSave_Click;
+            // 
+            // btnRead
+            // 
+            btnRead.Location = new Point(425, 329);
+            btnRead.Name = "btnRead";
+            btnRead.Size = new Size(123, 29);
+            btnRead.TabIndex = 7;
+            btnRead.Text = "Read";
+            btnRead.UseVisualStyleBackColor = true;
+            btnRead.Click += btnRead_Click;
+            // 
+            // btnDelete
+            // 
+            btnDelete.Location = new Point(607, 329);
+            btnDelete.Name = "btnDelete";
+            btnDelete.Size = new Size(123, 29);
+            btnDelete.TabIndex = 8;
+            btnDelete.Text = "Delete";
+            btnDelete.UseVisualStyleBackColor = true;
+            btnDelete.Click += btnDelete_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(btnDelete);
+            Controls.Add(btnRead);
+            Controls.Add(btnSave);
+            Controls.Add(btnNew);
+            Controls.Add(dataGridView1);
+            Controls.Add(txtMessage);
+            Controls.Add(txtTitle);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private TextBox txtTitle;
+        private TextBox txtMessage;
+        private DataGridView dataGridView1;
+        private Button btnNew;
+        private Button btnSave;
+        private Button btnRead;
+        private Button btnDelete;
+    }
+}
